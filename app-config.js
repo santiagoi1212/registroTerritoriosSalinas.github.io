61ef3b6a70dfe9463e25e38d2c60620f853aef75
santiagoi1212 <santiagoi1212@gmail.com>
2025-11-13 18:50:13 -0300

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,8 +2,12 @@
     window.APP_CONFIG = {
       USERS_CSV_URL: './users.csv',
       POLIGONOS_JSON_URL: './poligonos_salinas.json',
-      LOGIN_WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbwXu2ue7iLYsFtOMm8rEj6wEgcwEBYYcoiqlb3qpcYGtq0uanj5F9BXEUSupB-RJVg/exec',
+      AUTH_API_URL: 'https://script.google.com/macros/s/AKfycbwXu2ue7iLYsFtOMm8rEj6wEgcwEBYYcoiqlb3qpcYGtq0uanj5F9BXEUSupB-RJVg/exec',
       WEEKLY_API_URL: 'https://script.google.com/macros/s/AKfycbweQitKPLrgsjIcZP5tUbZBeov6_KLMcF9_SN-zaxvbsJFe-ekFMzeNQcRu9qWpYh8/exec?op=weekly',
-      SHEETS_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS-TxyDU1xvaMwDjc5GQxjglSfBUYTUyu2_NDcAsJ_v0ngaD8g_-WcmxsUd9921RF2q5I4bcscjsf6N/pub?gid=1159469350&single=true&output=csv',
-      WEBHOOK_URL: 'https://script.google.com/macros/s/AKfycbz3t0e03wMw6uJ-IsIIHmnvizMBRxeBF2Y7Et2uiTL-5d8L7di8uFA3HWkdw0xMLj0/exec'
-    };
+      REVISITAS_API_URL: "https://script.google.com/macros/s/AKfycbyN-FXlvI8lYzbZd7Rcg_D-tpTE0AdNUHsj7Bik689tjFYW1acsg-PVFZIttQ9evA4/exec",
+      SHEETS_TERRITORIOS_CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS-TxyDU1xvaMwDjc5GQxjglSfBUYTUyu2_NDcAsJ_v0ngaD8g_-WcmxsUd9921RF2q5I4bcscjsf6N/pub?gid=1159469350&single=true&output=csv',
+      WEBHOOK_URL: 'https://script.google.com/macros/s/AKfycbz3t0e03wMw6uJ-IsIIHmnvizMBRxeBF2Y7Et2uiTL-5d8L7di8uFA3HWkdw0xMLj0/exec',
+      REVISITAS_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTSvBzDVAWYEzWy73reop1_EtUdkXyGNq5KhpIMzbQLredIkKnJxTgstyHzPfZKhLI_23iggIlYXqUR/pub?gid=1217453780&single=true&output=csv",
+      NOVISITAR_CSV_URL: "https://script.google.com/macros/s/AKfycbyN-FXlvI8lYzbZd7Rcg_D-tpTE0AdNUHsj7Bik689tjFYW1acsg-PVFZIttQ9evA4/exec"
+};
+
